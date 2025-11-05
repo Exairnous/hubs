85f7db7ff7b7c4bc1075b72718242384fac475d9
Ryan Inch <mythologylover75@gmail.com>
2025-11-05 03:28:30 -0500

    test 4

--- src/utils/networked-video-schema.ts
+++ src/utils/networked-video-schema.ts
@@ -15,7 +15,7 @@ function apply(eid: EntityID, { version, data }: StoredComponent) {
     return true;
   } else if (version === 2) {
     const { time, flags, projection, src }: { time: number; flags: number; src: string; projection: number } = data;
-    console.log(`networked video apply raw src: ${src}`);
+    console.log(`networked video apply raw data: ${data}`);
     write(NetworkedVideo.time, eid, time);
     write(NetworkedVideo.flags, eid, flags);
     write(NetworkedVideo.projection, eid, projection);
